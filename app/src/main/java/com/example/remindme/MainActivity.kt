b579542fe1a856a92fece1485101ec2c61463662
Riyan-Mo <riyan8308151899@gmail.com>
2024-12-17 15:05:48 +0530

    Chore: update package path

--- app/src/main/java/com/example/remindme/MainActivity.kt
+++ app/src/main/java/com/example/remindme/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.example.compose.AppTheme
+import com.example.remindme.ui.theme.AppTheme
 import com.example.remindme.ui.RemindMeApp
 
 class MainActivity : ComponentActivity() {
