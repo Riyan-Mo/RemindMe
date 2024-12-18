1b2bd99abdbd6dba251cbf87d6223a20af034abb
Riyan-Mo <riyan8308151899@gmail.com>
2024-12-18 08:16:56 +0530

    Add room database

--- app/src/main/java/com/example/remindme/data/ReminderDatabase.kt
+++ app/src/main/java/com/example/remindme/data/ReminderDatabase.kt
@@ -1,6 +1,30 @@
 package com.example.remindme.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
-abstract class ReminderDatabaseRepository: Database() {
+@Database(entities = [Reminder::class], version = 1, exportSchema = false)
+abstract class ReminderDatabase: RoomDatabase() {
+
+    abstract fun dao(): ReminderDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ReminderDatabase? = null
+
+        fun getDatabase(context: Context): ReminderDatabase {
+            return INSTANCE?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ReminderDatabase::class.java,
+                    "reminder_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
 }
