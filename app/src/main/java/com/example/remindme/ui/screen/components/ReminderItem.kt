a86493624ca9935e2f2e054bbe54b9d47342fcb6
Riyan-Mo <riyan8308151899@gmail.com>
2024-12-24 10:38:18 +0530

    Chore: removed unused imports

--- app/src/main/java/com/example/remindme/ui/screen/components/ReminderItem.kt
+++ app/src/main/java/com/example/remindme/ui/screen/components/ReminderItem.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.remindme.data.Reminder
 import com.example.remindme.data.Type
-import java.time.format.DateTimeFormatter
 
 @Composable
 fun ReminderItem(
